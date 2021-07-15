51fd8857e986eb46f4cd4e16a4f1724490416e21
JakubVala <jamm3e3333@gmail.com>
2021-07-15 16:50:48 +0200

    css template done

--- public/js/app.js
+++ public/js/app.js
@@ -1,31 +1,46 @@
 const $btnSub = document.querySelector('#btn-sub');
 const $selectOpt = document.querySelector('#langs');
 const $textAr = document.querySelector('#txt_translate');
-const dataDiv = document.querySelector('#data-content');
+
+const dataDiv = document.querySelector('#data-main-content');
 const header = document.querySelector('.header');
 const fdiv = document.querySelector('.fdiv');
+const dataP = document.querySelector('.p-content');
 
 $btnSub.addEventListener('click', (e) => {
     e.preventDefault();
+
     if(!$textAr.value.trim()) {
         $textAr.value = '';
         $textAr.focus();
         return alert('You didn\'t write any text.');
     }
+
+    if($textAr.value.trim().includes('#')) {
+        $textAr.focus();
+        return alert('Character "#" is not allowed.');
+    }
+
+    dataP.setAttribute('style','display:none');
     e.target.setAttribute('disabled','disabled');
-    dataDiv.textContent = 'Loading...';
+    dataDiv.innerHTML = 'Loading...';
 
     const selOption = $selectOpt.options[$selectOpt.selectedIndex].value;
 
     makeReq(selOption, $textAr.value.trim())
         .then((data) => {
             e.target.removeAttribute('disabled');
+
             if(data.text && data.lang ) {
-                dataDiv.innerHTML = `<p>Translated text:</p> ${data.text}`;
+                dataP.setAttribute('style','display');
+                dataDiv.innerHTML = data.text;
             }
             else {
                 dataDiv.textContent = "Error catching data";
-            };
+            }
+
+            dataDiv.removeAttribute('style');
+
             window.scroll({
              top: header.offsetHeight + fdiv.offsetHeight - dataDiv.offsetHeight,
              behavior: 'smooth'
@@ -34,6 +49,14 @@ $btnSub.addEventListener('click', (e) => {
         .catch((err) => {
             e.target.removeAttribute('disabled');
             dataDiv.textContent = err.Error;
+
+            window.scroll({
+                top: header.offsetHeight + fdiv.offsetHeight - dataDiv.offsetHeight,
+                behavior: 'smooth'
+               });
+
+            dataDiv.removeAttribute('style');
+            
             console.log(err);
         });
 })
